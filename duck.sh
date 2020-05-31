4e686304b49db1b1de040a1441fafb5212d94224
Ivan <ivanspasic47@gmail.com>
2020-05-31 18:23:19 +0200

    Cleaner log messages

--- duck.sh
+++ duck.sh
@@ -4,10 +4,19 @@ cd /home/ivan/duckdns
 
 check=$(echo url="https://www.duckdns.org/update?domains=spasoye&token=5dec56a1-ddb0-4c7b-bda1-5d22ca1cc6d0&ip=" | curl -k -K -)
 
-echo $check
+# echo $check
+
+if [ $check == "OK" ]
+then
+    msg="PASS"
+else
+    msg="FAIL"
+fi
+
+echo $msg
 
 # Log the output and the timestamp
-echo "`{ date +"%D %T"; echo $check; } | tr "\n" " "`" >> duck.log
+echo "`{ date +"%D %T"; echo $msg; } | tr "\n" " "`" >> duck.log
 
 if [ "$check" == "OK" ]; then
     exit 0
